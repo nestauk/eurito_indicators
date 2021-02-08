b7a8642fe22f8acae19d12e04e1abf065da1c63d
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2021-02-08 18:44:12 -0300

    Minimal `svelte-media` setup

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,14 +1,25 @@
 <script>
+	import watchMedia from "svelte-media";
 	import Nav from 'app/components/Nav.svelte';
 
 	export let segment;
+
+	const mediaqueries = {
+		mobile: "(max-width: 639px)",
+		tablet: "(min-width: 640px) and (max-width: 1023px)",
+		desktop: "(min-width: 1024px)",
+		landscape: "(orientation: landscape)"
+	};
+	const media = watchMedia(mediaqueries);
+
+	$: console.log($media);
 </script>
 
-<header>
-	<Nav {segment}/>
+<header class={$media.classNames}>
+	<Nav {segment} media={$media}/>
 </header>
 
-<main>
+<main class={$media.classNames}>
 	<slot></slot>
 </main>
 
