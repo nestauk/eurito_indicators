34421b688c4cbd67c523c03530fca461a1839a8e
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2021-02-15 18:11:42 -0300

    Testing length units for font-sizes

---
+++ src/routes/font-test.svelte
@@ -0,0 +1,101 @@
+<section>
+	<h1>Font Size Tester</h1>
+	<div>
+		<p>
+			Default font size.
+		</p>
+		<p>
+			mmmmm mmmmm mmmmm mmmmm mmmmm mmmmm mmmmm 
+		</p>
+		<p>
+			xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx 
+		</p>	
+	</div>
+	
+	<hr>
+	<div class='pt'>
+		<p>
+			Font-size in pt.
+		</p>
+		<p>
+			mmmmm mmmmm mmmmm mmmmm mmmmm mmmmm mmmmm 
+		</p>
+		<p>
+			xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx 
+		</p>	
+	</div>
+	
+	<hr>
+	<div class='cm'>
+		<p>
+			Font-size in cm.
+		</p>
+		<p>
+			mmmmm mmmmm mmmmm mmmmm mmmmm mmmmm mmmmm 
+		</p>
+		<p>
+			xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx 
+		</p>	
+	</div>
+	
+	<hr>
+	<div class='px'>
+		<p>
+			Font-size in px.
+		</p>
+		<p>
+			mmmmm mmmmm mmmmm mmmmm mmmmm mmmmm mmmmm 
+		</p>
+		<p>
+			xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx 
+		</p>	
+	</div>
+	
+	<hr>
+	<div class='vw'>
+		<p>
+			Font-size in vw.
+		</p>
+		<p>
+			mmmmm mmmmm mmmmm mmmmm mmmmm mmmmm mmmmm 
+		</p>
+		<p>
+			xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx 
+		</p>	
+	</div>
+	
+	<hr>
+	<div class='vh'>
+		<p>
+			Font-size in vh.
+		</p>
+		<p>
+			mmmmm mmmmm mmmmm mmmmm mmmmm mmmmm mmmmm 
+		</p>
+		<p>
+			xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx 
+		</p>	
+	</div>
+</section>
+
+<style>
+	section {
+		height: 100%;
+		overflow-y: auto;
+	}
+	.cm * {
+		font-size: 0.4cm;
+	}
+	.px * {
+		font-size: 14px;
+	}
+	.pt * {
+		font-size: 12pt;
+	}
+	.vw * {
+		font-size: 4vw;
+	}
+	.vh * {
+		font-size: 2vh;
+	}
+</style>
