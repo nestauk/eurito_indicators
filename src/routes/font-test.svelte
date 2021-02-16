878ed40fd005ea1cf2c13300e6dd1d9be1efbc39
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2021-02-16 04:14:39 -0300

    Added measurement of screen size in units of letter 'x'

--- src/routes/font-test.svelte
+++ src/routes/font-test.svelte
@@ -1,101 +1,94 @@
-<section>
-	<h1>Font Size Tester</h1>
-	<div>
-		<p>
-			Default font size.
-		</p>
-		<p>
-			mmmmm mmmmm mmmmm mmmmm mmmmm mmmmm mmmmm 
-		</p>
-		<p>
-			xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx 
-		</p>	
-	</div>
+<script>
+	import TextRuler from 'app/components/TextRuler.svelte';
+
+	const sizes = {
+		rem: '1rem',
+		pt: '12pt',
+		px: '16px',
+		cm: '.423cm',
+		vw: '2vw',
+		vh: '2vh',
+		vmin: '2vmin',
+		vmax: '2vmax',
+	}
+	const units = Object.keys(sizes);
 	
-	<hr>
-	<div class='pt'>
-		<p>
-			Font-size in pt.
+	let sizeMultiplier = 1.0;
+	let dim;
+
+	function updateDim (e) {
+		dim = e.detail;
+	}
+</script>
+<TextRuler on:resize={updateDim} fontSize={sizeMultiplier+'rem'}/>
+<main>
+	<div class="square"> </div>
+	<section style={`--size-mult: ${sizeMultiplier}`}>
+		<p class='info'>
+			DPPR: {dim?.pixelRatio.toPrecision(4)} W: {dim?.pixelWidth} H: {dim?.pixelHeight} CHARS: {dim?.charsPerLine.toFixed(0)}x{dim?.linesPerPage.toFixed(0)}
 		</p>
-		<p>
-			mmmmm mmmmm mmmmm mmmmm mmmmm mmmmm mmmmm 
-		</p>
-		<p>
-			xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx 
-		</p>	
-	</div>
-	
-	<hr>
-	<div class='cm'>
-		<p>
-			Font-size in cm.
-		</p>
-		<p>
-			mmmmm mmmmm mmmmm mmmmm mmmmm mmmmm mmmmm 
-		</p>
-		<p>
-			xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx 
-		</p>	
-	</div>
-	
-	<hr>
-	<div class='px'>
-		<p>
-			Font-size in px.
-		</p>
-		<p>
-			mmmmm mmmmm mmmmm mmmmm mmmmm mmmmm mmmmm 
-		</p>
-		<p>
-			xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx 
-		</p>	
-	</div>
-	
-	<hr>
-	<div class='vw'>
-		<p>
-			Font-size in vw.
-		</p>
-		<p>
-			mmmmm mmmmm mmmmm mmmmm mmmmm mmmmm mmmmm 
-		</p>
-		<p>
-			xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx 
-		</p>	
-	</div>
-	
-	<hr>
-	<div class='vh'>
-		<p>
-			Font-size in vh.
-		</p>
-		<p>
-			mmmmm mmmmm mmmmm mmmmm mmmmm mmmmm mmmmm 
-		</p>
-		<p>
-			xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx 
-		</p>	
-	</div>
-</section>
+		{#each units as unit}
+			<div style={`--font-size: ${sizes[unit]}`}>
+				<header>
+					{`font-size: calc(${sizes[unit]} * ${sizeMultiplier})`}
+				</header>
+				<p>
+					Fmxg.
+				</p>
+				<p class='unused'>
+					mmmmm mmmmm mmmmm mmmmm mmmmm mmmmm mmmmm 
+				</p>
+				<p class='unused'>
+					xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx 
+				</p>
+			</div>
+			<hr />
+		{/each}
+	</section>
+	<input type='range' bind:value={sizeMultiplier} min={0.2} max={3} step={0.01}/>
+</main>
 
 <style>
+	main {
+		display: grid;
+		grid-template-rows: 1fr min-content;
+		height: 100%;
+		overflow: hidden;
+	}
 	section {
 		height: 100%;
 		overflow-y: auto;
 	}
-	.cm * {
-		font-size: 0.4cm;
+
+	input {
+		width:100%;
 	}
-	.px * {
-		font-size: 14px;
+
+	header {
+		color: maroon;
 	}
-	.pt * {
-		font-size: 12pt;
+	.info {
+		background: maroon;
+		color: white;
 	}
-	.vw * {
-		font-size: 4vw;
+	div p {
+		font-size: calc(var(--font-size) * var(--size-mult))
 	}
-	.vh * {
-		font-size: 2vh;
+	.unused {
+		display: none;
 	}
+	hr {
+		margin: 4px;
+	}
+	.square {
+		position: fixed;
+		width: 50vw;
+		height: 50vh;
+		font-size: 3vmin;
+		background: #ffff0088;
+		z-index: -1;
+		top: 25vh;
+		left: 25vw;
+	}
+
 </style>
