1cc24505b8153a845eee5f73fe0501ab2c32a22e
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2021-02-26 10:19:10 -0300

    Basic testing mechanics in place

--- test/BrS/scripts/test.mjs
+++ test/BrS/scripts/test.mjs
@@ -1,3 +1,7 @@
+import {capitalize} from '@svizzle/utils';
+
+import Queue from "queue-promise";
+import Caps from 'browserstack-capabilities';
 import webdriver from 'selenium-webdriver';
 import config from '../.config.mjs';
 
@@ -7,27 +11,41 @@ const {
 	url
 } = config.browserstack;
 
-let browserstackURL = `https://${username}:${key}@${url}`;
+const bsCapabilities = Caps(username, key);
+const devicesCaps = bsCapabilities.create();
 
-let capabilities = {
-	os : 'Windows',
-	os_version : '10',
-	browserName : 'Chrome',
-	browser_version : '80',
-	name : "sebastianferreyr1's First Test"
-}
+// convert to Selenium 4 format
+const s4caps = devicesCaps.map(deviceCaps => ({
+	device: deviceCaps.device,
+	browserName: capitalize(deviceCaps.browser),
+	browserVersion: deviceCaps.browser_version,
+	'bstack:options': {
+		os: deviceCaps.os,
+		osVersion: deviceCaps.os_version,
+		local: true,
+	}
+}));
 
-let driver = new webdriver.Builder()
-.usingServer(browserstackURL)
-.withCapabilities(capabilities)
-.build();
+console.log(s4caps.length)
+/*
+const browserstackURL = `https://${username}:${key}@${url}`;
 
-async function run () {
-	await driver.get('http://www.google.com');
-	await driver.findElement(webdriver.By.name('q')).sendKeys('BrowserStack');
-    const title = await driver.getTitle();
-	console.log(title);
+async function run (capabilities) {
+	const driver = new webdriver.Builder()
+	.usingServer(browserstackURL)
+	.withCapabilities(capabilities)
+	.build();
+
+	await driver.get('http://localhost:3000/font-test');
+	const info = await driver.findElement({id:'info'});
+	const text = await info.getText();
+	console.log(text);
 	driver.quit();
 }
 
-run();
+const queue = new Queue({
+	concurrent: 5,
+	interval: 30000
+  });
+s4caps.forEach(caps => queue.enqueue(() => run(caps)));
+*/
