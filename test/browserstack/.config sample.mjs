9cf69cc5700239fa4ce3b488027f3778bcac3246
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2021-03-09 04:14:57 -0300

    Progress

--- test/browserstack/.config sample.mjs
+++ test/browserstack/.config sample.mjs
@@ -2,6 +2,7 @@ export default {
 	browserstack: {
 		username: "your username",
 		key: "your key",
-		url: "hub-cloud.browserstack.com/wd/hub"
+		url: "hub-cloud.browserstack.com/wd/hub",
+		deployment: 'https://some-server.com'
 	}
 }
---
+++ test/browserstack/.config.mjs
@@ -0,0 +1,10 @@
+export default {
+	browserstack: {
+		username: 'sebastianferreyr1',
+		key: 'fD4wvrKEDzaUczFKbLTi',
+		url: 'hub-cloud.browserstack.com/wd/hub',
+		tests: 'test/browserstack/scripts/automate',
+		target: 'https://deploy-preview-15--eurito-indicators-ui-dev.netlify.app/guide',
+		report: 'report.json'
+	}
+}
