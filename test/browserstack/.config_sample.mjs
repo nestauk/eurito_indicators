b064abef68a9ba99ec51f3bb5caf0b64af85364b
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2021-03-11 13:13:12 -0300

    Small fix

--- test/browserstack/.config_sample.mjs
+++ test/browserstack/.config_sample.mjs
@@ -5,6 +5,6 @@ export default {
 		url: "hub-cloud.browserstack.com/wd/hub",
 		tests: 'test/browserstack/scripts/automate',
 		target: 'https://deploy-preview-21--eurito-indicators-ui-dev.netlify.app',
-		report: 'test/selenium-report.json'
+		report: 'data/tests/selenium-report.json'
 	}
 }
