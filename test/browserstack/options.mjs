4f24913c828c7aa79fb58744ade458f94f91202a
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2021-03-24 16:20:16 -0300

    Only one resolution enabled to speed up testing.
    Also, only one resolution needed for accessibility statement

--- test/browserstack/options.mjs
+++ test/browserstack/options.mjs
@@ -100,15 +100,15 @@ export const devices = [
 
 const windowsResolutions = [
 	'1024x768',
-	'1280x800',
-	'1280x1024',
-	'1366x768',
-	'1440x900',
-	'1680x1050',
-	'1600x1200',
-	'1920x1200',
-	'1920x1080',
-	'2048x1536'
+	// '1280x800',
+	// '1280x1024',
+	// '1366x768',
+	// '1440x900',
+	// '1680x1050',
+	// '1600x1200',
+	// '1920x1200',
+	// '1920x1080',
+	// '2048x1536'
 ];
 export const windowsResolutionsOlder = ['800x600', ...windowsResolutions];
 const osxResolutions = [
