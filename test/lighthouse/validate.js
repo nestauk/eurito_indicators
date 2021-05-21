753c850915f189d7d6b2379ca5468b6c4df99a0b
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2021-05-21 00:21:49 -0300

    Various minor changes and new reports

--- test/lighthouse/validate.js
+++ test/lighthouse/validate.js
@@ -2,14 +2,15 @@ import fs from 'fs';
 import lighthouse from 'lighthouse';
 import * as chromeLauncher from 'chrome-launcher';
 
-import {lighthouseUrls} from '../../src/node_modules/app/config';
+import {lighthouseUrls, urlBases} from '../../src/node_modules/app/config';
 
 (async () => {
 	for (let [id, url] of Object.entries(lighthouseUrls)) {
 		const chrome = await chromeLauncher.launch({
 			chromeFlags: [
 				'--headless',
-				// '--no-sandbox',  // uncomment if there are problems running the tests
+				// uncomment below if there are problems running the tests
+				// '--no-sandbox',
 			]
 		});
 		const options = {
@@ -18,15 +19,24 @@ import {lighthouseUrls} from '../../src/node_modules/app/config';
 			onlyCategories: ['accessibility'],
 			port: chrome.port
 		};
-		const runnerResult = await lighthouse(url, options);
-		const reportHtml = runnerResult.report;	
+		const runnerResult = await lighthouse(
+			urlBases.development + url,
+			options
+		);
+		const reportHtml = runnerResult.report.replaceAll(urlBases.development, '');
 
 		// eslint-disable-next-line no-sync
 		fs.writeFileSync(`static/a11y/${id}.html`, reportHtml);
 
 		// `.lhr` is the Lighthouse Result as a JS object
-		console.log('Report is done for', runnerResult.lhr.finalUrl);
-		console.log('Performance score was', runnerResult.lhr.categories.accessibility.score * 100);
+		console.log(
+			'Report is done for',
+			runnerResult.lhr.finalUrl
+		);
+		console.log(
+			'Performance score was',
+			runnerResult.lhr.categories.accessibility.score * 100
+		);
 
 		await chrome.kill();
 	}
