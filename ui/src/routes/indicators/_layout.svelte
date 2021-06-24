637d7e2a43ac0693b356be68d2f5ea1902eeefc6
Luca Bonavita <mindrones@users.noreply.github.com>
2021-06-24 10:02:23 +0100

    Upgrade svizzles
    Closes #127

--- ui/src/routes/indicators/_layout.svelte
+++ ui/src/routes/indicators/_layout.svelte
@@ -1,6 +1,8 @@
 <script>
 	import Layout from '@svizzle/time_region_value/src/routes/_layout.svelte';
 
+	import {flags, regionSettings} from 'app/config';
+	import POIs from 'app/data/majorCities';
 	import {_groups} from 'app/stores/data';
 	import theme from 'app/theme';
 
@@ -9,6 +11,9 @@
 
 <Layout
 	{_groups}
+	{flags}
+	{POIs}
+	{regionSettings}
 	{segment}
 	{theme}
 	hrefBase='indicators'
