eb25552c0d996e5a5988dd82b9d4713e215a2c2f
Juan-Mateos <juan.mateos-garcia@nesta.org.uk>
2021-07-06 10:05:14 +0100

    add script to make cordis tables

--- ds/eurito_indicators/pipeline/make_cordis.py
+++ ds/eurito_indicators/pipeline/make_cordis.py
@@ -12,9 +12,9 @@ import pandas as pd
 import requests
 
 from eurito_indicators import config, PROJECT_DIR
-from eurito_indicators.getters.covid_getters import get_cordis_labelled
 from eurito_indicators.pipeline.processing_utils import covid_getter
 
+LEVEL_LOOKUP = config["covid_level_names"]
 POST_PATH = f"{PROJECT_DIR}/inputs/data/postcode_nuts_lookup"
 
 
@@ -65,13 +65,33 @@ def fetch_cordis_projects() -> pd.DataFrame:
 
     return cordis_projects_clean
 
+def read_cordis_labelled() -> pd.DataFrame:
+    """Fetch cordis labelled dataset"""
+
+    covid_port = pd.read_excel(
+        f"{PROJECT_DIR}/inputs/data/covid_19_portfolio.xlsx",
+        sheet_name="Projects",
+        skiprows=1,
+    )
+    covid_port.columns = [re.sub(" ", "_", col.lower()) for col in covid_port.columns]
+
+    # Rename levels
+
+    covid_port["level_relabelled"] = covid_port["portfolio_level"].map(LEVEL_LOOKUP)
+
+    covid_level_lookup = covid_port.set_index("project_number")[
+        "level_relabelled"
+    ].to_dict()
+
+    return covid_level_lookup
+
 
 def make_cordis_projects() -> pd.DataFrame:
     """Read the cordis data and enrich with Covid-related information"""
 
     cordis_projects = fetch_cordis_projects()
 
-    cordis_level_lookup = get_cordis_labelled()
+    cordis_level_lookup = read_cordis_labelled()
 
     cordis_projects["covid_level"] = cordis_projects["project_id"].map(
         cordis_level_lookup
@@ -156,17 +176,6 @@ def fetch_cordis_organisations():
     return orgs
 
 
-def make_extra_nuts(cordis_orgs):
-    """Adds extra nuts codes by slicing the nuts3 code we already obtained"""
-
-    for n, name in enumerate(["nuts2", "nuts1"]):
-        cordis_orgs[name] = cordis_orgs["nuts3"].apply(
-            lambda x: x[: (-n - 1)] if pd.isnull(x) == False else np.nan
-        )
-
-    return cordis_orgs
-
-
 def make_cordis_organisations():
     """Fetch cordis organisations and postcode lookuo"""
 
@@ -182,9 +191,6 @@ def make_cordis_organisations():
         right_on=["country", "postcode"],
         how="left",
     )
-
-    orgs_geo = make_extra_nuts(orgs_geo)
-
     return orgs_geo
 
 
