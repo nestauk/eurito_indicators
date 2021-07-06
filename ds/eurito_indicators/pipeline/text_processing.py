86f036430ad063347f2dc90cf720f9b4a134594c
Juan-Mateos <juan.mateos-garcia@nesta.org.uk>
2021-07-06 14:37:55 +0100

    added scripts to process text, supervised prediction and topic modelling

--- ds/eurito_indicators/pipeline/text_processing.py
+++ ds/eurito_indicators/pipeline/text_processing.py
@@ -1,7 +1,6 @@
 # Text processing functions
 
 import logging
-import re
 import string
 from itertools import chain
 
@@ -11,18 +10,15 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
 stem = WordNetLemmatizer()
-STOP = set(stopwords.words("english"))
-BAD = set([x for x in string.punctuation + string.digits if x != "-"])
+STOP = stopwords.words("english")
+BAD = [x for x in string.punctuation + string.digits] + ["\n"]
+BAD.remove("-")
 
 
-def pre_process(text, count=None, decs=1e4):
+def pre_process(text):
     """Removes stopwords and symbols and lemmatises"""
 
-    if count is not None:
-        if count % decs == 0:
-            logging.info(count)
-
-    lowercase = re.sub("\n", " ", text.lower())
+    lowercase = text.lower()
     no_numbers_symbols = "".join([x for x in lowercase if x not in BAD])
     tokenised = [x for x in no_numbers_symbols.split(" ") if x not in STOP]
     lemmatised = [stem.lemmatize(x) for x in tokenised if len(x) > 2]
@@ -54,7 +50,7 @@ def remove_extr_freq(corpus, high=0.999, low=5):
     drop_high = freqs[freqs > very_high].index.tolist()
     drop_low = freqs[freqs < low].index.tolist()
 
-    todrop = set(drop_high + drop_low)
+    todrop = drop_high + drop_low
 
     corpus_filt = [[x for x in doc if x not in todrop] for doc in corpus]
 
