4dfe3230d39069588d65860a85d0247baf42cf24
Juan-Mateos <juan.mateos-garcia@nesta.org.uk>
2021-07-06 14:48:08 +0100

    added script to fit a doc2vec model on the cordis data

--- ds/eurito_indicators/pipeline/fit_doc2vec_cordis.py
+++ ds/eurito_indicators/pipeline/fit_doc2vec_cordis.py
@@ -5,31 +5,25 @@ import logging
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 
 from eurito_indicators.getters.cordis_getters import get_cordis_projects
-from eurito_indicators.pipeline.processing_utils import (
-    cordis_combine_text,
-    filter_by_length,
-    save_model,
-)
+from eurito_indicators.pipeline.processing_utils import cordis_combine_text, save_model
 from eurito_indicators.pipeline.text_processing import make_engram, pre_process
 
 
 if __name__ == "__main__":
 
     logging.info("Reading and preprocessing data")
-    cordis_corpus = get_cordis_projects()
-    cordis_corpus_long = filter_by_length(cordis_corpus, "objective", min_length=500)
 
-    cordis_tokenised = make_engram(
-        [pre_process(doc) for doc in cordis_corpus_long["objective"]]
-    )
+    cordis_corpus = cordis_combine_text(get_cordis_projects())
+
+    cordis_tokenised = make_engram([pre_process(doc) for doc in cordis_corpus["text"]])
 
     logging.info("Training model")
     train_corpus = [
         TaggedDocument(words=tok, tags=[str(_id)])
-        for tok, _id in zip(cordis_tokenised, cordis_corpus_long["project_id"])
+        for tok, _id in zip(cordis_tokenised, cordis_corpus["project_id"])
     ]
 
-    model = Doc2Vec(vector_size=200, min_count=5, epochs=200)
+    model = Doc2Vec(vector_size=100, min_count=5, epochs=200)
     model.build_vocab(train_corpus)
     model.train(train_corpus, total_examples=model.corpus_count, epochs=model.epochs)
 
