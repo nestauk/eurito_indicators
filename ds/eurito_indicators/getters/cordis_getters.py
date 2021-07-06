fa45703bb69d51c3d2ff62efb6d375a5dd6fe794
Juan-Mateos <juan.mateos-garcia@nesta.org.uk>
2021-07-06 16:16:04 +0100

    moved cordis labelled getter to getter script

--- ds/eurito_indicators/getters/cordis_getters.py
+++ ds/eurito_indicators/getters/cordis_getters.py
@@ -1,5 +1,6 @@
 # cordis data getters
 
+import pickle
 from datetime import datetime
 
 import pandas as pd
@@ -15,3 +16,28 @@ def get_cordis_projects():
 def get_cordis_organisations():
 
     return pd.read_csv(f"{PROJECT_DIR}/inputs/data/cordis_organisations.csv")
+
+def get_cordis_labelled() -> pd.DataFrame:
+    """Fetch cordis labelled dataset"""
+
+    covid_port = pd.read_excel(
+        f"{PROJECT_DIR}/inputs/data/covid_19_portfolio.xlsx",
+        sheet_name="Projects",
+        skiprows=1,
+    )
+    covid_port.columns = [re.sub(" ", "_", col.lower()) for col in covid_port.columns]
+
+    # Rename levels
+
+    covid_port["level_relabelled"] = covid_port["portfolio_level"].map(LEVEL_LOOKUP)
+
+    covid_level_lookup = covid_port.set_index("project_number")[
+        "level_relabelled"
+    ].to_dict()
+
+    return covid_level_lookup
+
+
+def get_doc2vec():
+    with open(f"{PROJECT_DIR}/outputs/models/doc2vec_cordis.p", 'rb') as infile:
+        return pickle.load(infile)
