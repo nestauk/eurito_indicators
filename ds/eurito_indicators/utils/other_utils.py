38bbb2dc3d7eb897b9ab505e12717cc08cf2895a
Juan-Mateos <juan.mateos-garcia@nesta.org.uk>
2021-07-14 07:15:40 +0100

    miscelaneous utils

--- ds/eurito_indicators/utils/other_utils.py
+++ ds/eurito_indicators/utils/other_utils.py
@@ -1,20 +1,13 @@
-import json
 import matplotlib.pyplot as plt 
 
 from eurito_indicators import PROJECT_DIR
 
 FIG_PATH = f"{PROJECT_DIR}/outputs/figures"
 
-def save_matplot(name, path=FIG_PATH):
+def save_matplot(name):
     plt.tight_layout()
-    plt.savefig(path+f"/{name}.png", dpi=1000)
+    plt.savefig(FIG_PATH+f"/{name}.pdf")
 
 def save_lookup(file, name):
     with open(f"{PROJECT_DIR}/inputs/data/{name}.json", 'w') as outfile:
         json.dump(file,outfile)
-
-def clean_table(table, columns, clean_lookup):
-    t = table.copy()
-    for c in columns:
-        t[f"{c}_clean"] = t[c].map(clean_lookup)
-    return t
