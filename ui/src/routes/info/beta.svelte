9f383e7c9114c44b7620a90c61ab6dd3a617ce3a
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2021-07-26 22:59:44 -0300

    Added `/info` route showing meta content
    Closes #164

---
+++ ui/src/routes/info/beta.svelte
@@ -0,0 +1,36 @@
+<section>
+	<h2>Beta release</h2>
+	<p>
+		Welcome to the EURITO tool. The tool is currently in a Beta release and
+		this build will remain unmaintained over the course of the summer to
+		enable feedback to be gathered from live users.
+	</p>
+	
+	<p>
+		Please use the feedback form provided [here](/info) in the Feedback tab,
+		or alternatively email your feedback to dataanalytics@nesta.org.uk.
+	</p>
+
+	<p>Thanks!</p>
+</section>
+
+<style>
+	section {
+		background-color: white;
+		box-shadow: var(--box-shadow-y);
+		max-width: 900px;
+		overflow-y: auto;
+		padding: 2rem;
+	}
+	h2 {
+		font-weight: normal;
+		margin-bottom: 1.5rem;
+		margin-top: 1.5rem;
+	}
+	p {
+		margin: 0.75rem 0;
+	}
+	ul {
+		padding: 0.5rem 0 0.5rem 2rem;
+	}
+</style>
---
+++ ui/src/routes/info/privacy.svelte
@@ -0,0 +1,44 @@
+<section>
+	<h2>Cookies and data privacy policy</h2>
+	<p>
+		Cookies are files saved on your phone, tablet or computer when you visit
+		a website.
+	</p>
+	<p>
+		We only use essential cookies on this website:
+	</p>
+	
+	<ul>
+		<li>the tool itself does not collect any personal data.</li>
+		<li>
+			the feedback form is using Google Forms, which installs cookies in
+			your browser.
+		</li>
+	</ul>
+
+	<p>
+		By submitting data with Google Forms you accept the Terms of Service
+		and the Data Privacy Policy of Google Inc.
+	</p>
+</section>
+
+<style>
+	section {
+		background-color: white;
+		box-shadow: var(--box-shadow-y);
+		max-width: 900px;
+		overflow-y: auto;
+		padding: 2rem;
+	}
+	h2 {
+		font-weight: normal;
+		margin-bottom: 1.5rem;
+		margin-top: 1.5rem;
+	}
+	p {
+		margin: 0.75rem 0;
+	}
+	ul {
+		padding: 0.5rem 0 0.5rem 2rem;
+	}
+</style>
