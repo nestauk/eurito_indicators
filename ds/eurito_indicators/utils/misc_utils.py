eecfcdd7bcbf03e614de4e05ea8df222b54c1060
George Richardson <georgerichardson@posteo.net>
2021-07-28 08:13:15 +0100

    Add camel to snake

--- ds/eurito_indicators/utils/misc_utils.py
+++ ds/eurito_indicators/utils/misc_utils.py
@@ -6,7 +6,7 @@ def camel_to_snake(name):
     Args:
         name (str): lowerCamelCase or upperCamelCase word.
     Returns:
-        _name (str): snake_case word.
+        (str): snake_case word.
     '''
     s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
