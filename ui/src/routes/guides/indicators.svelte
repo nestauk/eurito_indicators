675e9be2023e12ec20398e12facc241505028e41
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2021-09-08 16:16:23 -0300

    Extracted CTA blocks from indicators guides and now using Code component

--- ui/src/routes/guides/indicators.svelte
+++ ui/src/routes/guides/indicators.svelte
@@ -1,11 +1,15 @@
 <script>
 	import {_screen} from '@svizzle/ui/src/gauges/screen/ScreenGauge.svelte';
+	import CTA from 'app/components/CTA.svelte';
 	import IndicatorsMedium
 		from 'app/components/content/guides/IndicatorsMedium.svx';
 	import IndicatorsSmall
 		from 'app/components/content/guides/IndicatorsSmall.svx';
+		import LinkButton from '@svizzle/ui/src/LinkButton.svelte';
 
 	import {toolName} from 'app/config';
+	import theme from 'app/theme';
+
 </script>
 
 <svelte:head>
@@ -16,8 +20,32 @@
 	/>
 </svelte:head>
 
-{#if $_screen?.sizes.medium}
-	<IndicatorsMedium />
-{:else}
-	<IndicatorsSmall />
-{/if}
+<div>
+	{#if $_screen?.sizes.medium}
+		<IndicatorsMedium />
+	{:else}
+		<IndicatorsSmall />
+	{/if}
+
+	<CTA>
+		<LinkButton
+			href='/accessibility'
+			text='Read the accessibility statement'
+			theme={{backgroundColor: theme.colorLink}}
+		/>
+		<LinkButton
+			href='/indicators'
+			text='Explore the indicators'
+			theme={{backgroundColor: theme.colorLink}}
+		/>
+	</CTA>
+</div>
+
+<style>
+	div {
+		background-color: white;
+		max-width: 900px;
+		overflow-y: auto;
+		padding: 2rem;
+	}
+</style>
