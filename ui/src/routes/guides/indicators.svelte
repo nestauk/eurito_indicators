bff5fc72af5d22dd7e389a2b710522191b44cfbe
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2021-09-10 12:05:05 -0300

    Moved CTA buttons to `routes/guides/_layout.svelte`

--- ui/src/routes/guides/indicators.svelte
+++ ui/src/routes/guides/indicators.svelte
@@ -1,12 +1,9 @@
 <script>
 	import {_screen} from '@svizzle/ui/src/gauges/screen/ScreenGauge.svelte';
-	import LinkButton from '@svizzle/ui/src/LinkButton.svelte';
 	import IndicatorsMedium from '../_content/guides/IndicatorsMedium.svx';
 	import IndicatorsSmall from '../_content/guides/IndicatorsSmall.svx';
-	import ResponsiveFlex from 'app/components/ResponsiveFlex.svelte';
 
 	import {toolName} from 'app/config';
-	import theme from 'app/theme';
 </script>
 
 <svelte:head>
@@ -23,19 +20,6 @@
 	{:else}
 		<IndicatorsSmall />
 	{/if}
-
-	<ResponsiveFlex>
-		<LinkButton
-			href='/accessibility'
-			text='Read the accessibility statement'
-			theme={{backgroundColor: theme.colorLink}}
-		/>
-		<LinkButton
-			href='/indicators'
-			text='Explore the indicators'
-			theme={{backgroundColor: theme.colorLink}}
-		/>
-	</ResponsiveFlex>
 </div>
 
 <style>
